44b0bec17b92720b54709cdd8114946de3ab1e6d
dzxcln <rohailshehroz@gmail.com>
2025-09-20 14:39:54 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
-print("My name is Rohail.")
-print("I am 11 years old.")
-print("I am in a school called Al Forsan.")
+#Welcome To The World Of Coding!
+    
+print("Welcome to the world of programming!")    
+print(4)
+print("hello, 5")
+print("Welcome to", end="*")
+x = 5
+y = "Rohail"
+print({x})
+print({y})
+codingal = "Penguin"
+number = 1
+print({codingal})
+print({number})
+name = input("Enter your name")
+print("My name is ", {name})
+
+#Keywords
+import keyword
+print("Python Keywords Are...\n")
+print(keyword.kwlist)
