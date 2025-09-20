1ce2dedf1523f4ea65ea6cfc241cc7a6e2424f33
dzxcln <rohailshehroz@gmail.com>
2025-09-20 14:47:44 +0300

    Add files via upload

---
+++ Storing Birthdays.py
@@ -0,0 +1,15 @@
+Dad = input("What is your dads birthday?")
+Mom = input("What is your moms birthday?")
+Sister = input("What is your sisters birthday?")
+Brother = input("What is your brothers birthday?")
+You = input("What is your birthday?")
+
+print("Your Dads birthday is ", {Dad})
+
+print("Your Moms birthday is ", {Mom})
+
+print("Your Sisters birthday is ", {Sister})
+
+print("Your Brothers birthday is ", {Brother})
+
+print("Your birthday is ", {You})
